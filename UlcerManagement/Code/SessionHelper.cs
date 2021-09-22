abe9a7eb4071dcc256ec6ebc62f3c77f031a7c62
khanhdc <46121994+khanhdc@users.noreply.github.com>
2021-09-22 03:18:28 -0700

    Add files via upload

---
+++ UlcerManagement/Code/SessionHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UlcerManagement.Code
+{
+    public class SessionHelper
+    {
+        public static void SetSession(UserSession session)
+        {
+            HttpContext.Current.Session["loginSession"] = session;
+        }
+        public static UserSession GetSession()
+        {
+            var session = HttpContext.Current.Session["loginSession"];
+            if (session == null)
+                return null;
+            else
+                return session as UserSession;
+        }
+        public bool SessionDirectionCheck(int[] groupID)
+        {
+            UserSession session = new UserSession();
+            session = (UserSession)System.Web.HttpContext.Current.Session["loginSession"];
+            if (session != null)
+            {
+                if (groupID.Contains(session.groupID))
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        public bool SessionDirectionCheck(int groupID)
+        {
+            UserSession session = new UserSession();
+            session = (UserSession)System.Web.HttpContext.Current.Session["loginSession"];
+            if (session != null)
+            {
+                if (groupID == session.groupID)
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ UlcerManagement/Code/UserSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UlcerManagement.Code
+{
+    [Serializable]
+    public class UserSession
+    {
+        public string userName { get; set; }
+        public int userID { get; set; }
+        public int groupID { get; set; }
+        public int addedBy { get; set; }
+        public string  fullName { get; set; }
+        public string profileImage { get; set; }
+    }
+}
